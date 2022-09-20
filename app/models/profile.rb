7c255f09fca4e4d8a960dbc411ef7b4c7ab01d57
Gegardus <stgegard@gmail.com>
2022-09-20 13:01:44 +0400

    add profilable

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,9 +2,5 @@ class Profile < ApplicationRecord
   # belongs_to :doctor
   # belongs_to :patient
   # belongs_to :admin
-  belongs_to :profileable, polymorphic: true
-
-  def role
-    profile.profile_type.downcase   # 'admin', 'doctor' or 'patient'
- end
+  belongs_to :profileable, polymorphic: true 
 end
