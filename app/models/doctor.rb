18612ec86d325e97cf934dd0c7c90740a4daf305
Gegardus <stgegard@gmail.com>
2022-09-21 09:36:08 +0400

    add category

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,4 +2,5 @@ class Doctor < ApplicationRecord
   has_many :appointments
   has_many :patients, through: :appointments
   has_one :profile
+  has_one :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_19_050810) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_21_053318) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_050810) do
     t.datetime "updated_at", null: false
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.bigint "doctor_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_categories_on_doctor_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -55,9 +63,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_050810) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "profileable_type"
+    t.bigint "profileable_id"
     t.index ["admin_id"], name: "index_profiles_on_admin_id"
     t.index ["doctor_id"], name: "index_profiles_on_doctor_id"
     t.index ["patient_id"], name: "index_profiles_on_patient_id"
+    t.index ["profileable_type", "profileable_id"], name: "index_profiles_on_profileable"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,6 +88,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_19_050810) do
 
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "patients"
+  add_foreign_key "categories", "doctors"
   add_foreign_key "profiles", "admins"
   add_foreign_key "profiles", "doctors"
   add_foreign_key "profiles", "patients"
